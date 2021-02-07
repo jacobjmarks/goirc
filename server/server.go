c43bfbce9d6d0c0ef38e48c45ed0281afe7ab345
Jacob Marks <jacobjmarks@outlook.com>
2021-02-07 20:53:18 +1000

    Update client store to utilise map
    Broadcast client connect/disconnect

--- server/server.go
+++ server/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"net"
 )
@@ -17,12 +18,17 @@ type Client struct {
 	conn net.Conn
 }
 
+// RemoteAddr returns the remote address of the client
+func (c *Client) RemoteAddr() string {
+	return c.conn.RemoteAddr().String()
+}
+
 // Server with clients
 type Server struct {
 	network string
 	host    string
 	port    string
-	clients []*Client
+	clients map[*Client]*Client
 }
 
 func newServer(network string, host string, port string) *Server {
@@ -32,7 +38,7 @@ func newServer(network string, host string, port string) *Server {
 		network: network,
 		host:    host,
 		port:    port,
-		clients: []*Client{},
+		clients: make(map[*Client]*Client),
 	}
 
 	return &server
@@ -65,50 +71,45 @@ func (s *Server) handleConnection(conn net.Conn) {
 		conn: conn,
 	}
 
-	log.Printf("Client %v connected\n", client.conn.RemoteAddr().String())
+	log.Printf("Client %v connected\n", client.RemoteAddr())
 
 	s.enqueueClient(&client)
 	go s.handleClient(&client)
 }
 
-func (s *Server) enqueueClient(client *Client) {
-	// Find available client slot
-	for i, c := range s.clients {
-		if c == nil || c.conn == nil {
-			s.clients[i] = client
-			return
-		}
-	}
-
-	// No existing clients or all are active
-	s.clients = append(s.clients, client)
+func (s *Server) enqueueClient(c *Client) {
+	s.clients[c] = c
+	go s.broadcast([]byte(fmt.Sprintf("A user has connected to the server. There are now %v others here.\n", len(s.clients)-1)), c)
 }
 
 func (s *Server) handleClient(c *Client) {
+	c.conn.Write([]byte(fmt.Sprintf("Welcome. There are %v others currently here.\n", len(s.clients)-1)))
+
 	for {
 		buffer, err := bufio.NewReader(c.conn).ReadBytes('\n')
 
 		if err != nil {
-			log.Printf("Client %v disconnected\n", c.conn.RemoteAddr().String())
+			log.Printf("Client %v disconnected\n", c.RemoteAddr())
 			c.conn.Close()
-			c.conn = nil
+			delete(s.clients, c)
+			go s.broadcast([]byte(fmt.Sprintf("A user has disconnected from the server. There are now %v others here.\n", len(s.clients)-1)), nil)
 			return
 		}
 
-		s.receiveMessage(buffer, c)
+		go s.receiveMessage(buffer, c)
 	}
 }
 
 func (s *Server) receiveMessage(msg []byte, c *Client) {
 	// TODO: Validate payload
-	log.Printf("Received message from client %v: %v\n", c.conn.RemoteAddr().String(), string(msg[:len(msg)-1]))
-	s.broadcast(msg, c)
+	log.Printf("Received message from client %v: %v\n", c.RemoteAddr(), string(msg[:len(msg)-1]))
+	go s.broadcast(msg, c)
 }
 
 func (s *Server) broadcast(msg []byte, client *Client) {
 	log.Printf("Broadcasting message to %v clients: %v", len(s.clients), string(msg[:len(msg)-1]))
 	for _, c := range s.clients {
-		if c == client || c.conn == nil {
+		if c == client {
 			continue
 		}
 
@@ -119,7 +120,7 @@ func (s *Server) broadcast(msg []byte, client *Client) {
 func (s *Server) sendToClient(msg []byte, c *Client) {
 	_, err := c.conn.Write(msg)
 	if err != nil {
-		log.Printf("Error sending message to client %v: %v", c.conn.RemoteAddr().String(), err.Error())
+		log.Printf("Error sending message to client %v: %v", c.RemoteAddr(), err.Error())
 	}
 }
 
